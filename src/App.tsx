c7343c9b95eb15db55eb14934d5831584dd06c68
JoaquinPettinari <joaquinpettinari@hotmail.com>
2024-06-10 11:13:47 -0300

    2 try to test routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Article from "./pages/Article";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="articulo/:id" element={<Article />} />
-        <Route path="*" element={<p>Page not found</p>} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="articulo/:id" element={<Article />} />
+          <Route path="*" element={<p>Page not found</p>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
