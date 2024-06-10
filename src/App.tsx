33bc3910dc3ff81a100f7170f1a35f5a3f523609
JoaquinPettinari <joaquinpettinari@hotmail.com>
2024-06-10 11:10:25 -0300

    Testing to fix routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,18 @@
-import { RouteObject, useRoutes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
 import Article from "./pages/Article";
 
 function App() {
-  const routes: RouteObject[] = [
-    {
-      path: "/",
-      element: <Layout />,
-      children: [
-        { index: true, element: <Home /> },
-        {
-          path: "/articulo/:id",
-          element: <Article />,
-        },
-        { path: "*", element: <p>Page not found</p> },
-      ],
-    },
-  ];
-
-  const element = useRoutes(routes);
-
-  return <main>{element};</main>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="articulo/:id" element={<Article />} />
+        <Route path="*" element={<p>Page not found</p>} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
